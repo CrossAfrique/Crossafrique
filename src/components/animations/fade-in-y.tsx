d6cd25f06928e0518e9c238b0183741c3dff8a37
yomzy2223 <oluwolesayo10@gmail.com>
2025-03-25 01:06:34 +0100

    create reusable animation components and use in hero section

---
+++ src/components/animations/fade-in-y.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { motion } from "motion/react";
+import React, { type ReactNode, type ElementType } from "react";
+
+interface FadeInYProps {
+	children: ReactNode;
+	delay?: number;
+	className?: string;
+	as?: ElementType;
+}
+
+export default function FadeInY({
+	children,
+	delay,
+	className,
+	as: Component = motion.div,
+}: FadeInYProps) {
+	return (
+		<Component
+			initial={{ opacity: 0, y: 20 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 0.8, delay: delay || 0 }}
+			className={cn("flex flex-col sm:flex-row gap-4", className)}
+		>
+			{children}
+		</Component>
+	);
+}
