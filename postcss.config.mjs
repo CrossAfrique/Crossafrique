67d57dc4a5d0d9403a6aba178f537ce051384e2a
CrossAfrique <crossafriquehydrogen@gmail.com>
2025-07-24 16:22:34 +0000

    update postcs.config.mjs file

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,15 +1,4 @@
 const config = {
 	plugins: ["@tailwindcss/postcss"],
 };
-module.exports = {
-	theme: {
-		extend: {
-			prose: {
-				'p': { marginBottom: '1rem' },
-				'h1': { fontSize: '2rem', marginBottom: '1.5rem' },
-				// Add more styles as needed
-			},
-		},
-	},
-};
 export default config;
